25176e8ecbc5f796117241729c40adaa4ee41f87
Risabh Sharma <rishabh_sharma@nowhere.com>
2022-02-09 22:42:12 +0530

    Polished library.sh
    Pulled jump_if in a funtion of it's own
    Pulled error handling in a seperate function

--- library.sh
+++ library.sh
@@ -13,7 +13,6 @@ function addition () {
     
     if [[ -z "$first_number"  ]] || [[ -z "$second_number"  ]]
     then
-        echo "Addition : Array location you're trying to access was not intialized."
         return 1
     fi
 
@@ -25,6 +24,53 @@ function addition () {
     echo "${sprint_program[@]}"
 }
 
+function jump_if() {
+    local sprint_program=($1)
+    local current_index=$2
+    
+    local first_index=$(( $current_index + 1 ))
+    local second_index=$(( $current_index + 2 ))
+    local third_index=$(( $current_index + 3 ))
+    
+    local first_number=${sprint_program[${sprint_program[$first_index]}]}
+    local second_number=${sprint_program[${sprint_program[$second_index]}]}
+
+    if [[ -z "$first_number"  ]] || [[ -z "$second_number"  ]]
+    then
+        return 2
+    fi
+    
+    current_index=$(( ${current_index} + 3 ))
+    if [[ ${first_number} -lt ${second_number} ]]
+    then
+        current_index=${sprint_program[$third_index]}
+    fi
+
+    if [[ -z ${sprint_program[$current_index]} ]]
+    then
+        return 3
+    fi
+    
+    echo $current_index
+}
+
+function error_handler () {
+    local error_status=$1
+    if [[ $error_status -eq 1 ]]
+    then
+        echo "Addition : Array location you're trying to access is not intialized."
+    fi
+    if [[ $error_status -eq 2 ]]
+    then
+        echo "Jump_if : Array location you're trying to access is not intialized."
+    fi
+    if [[ $error_status -eq 3 ]]
+    then
+        echo "Jump_if : Cannot jump to the requested location as it is not initialized."
+    fi
+}
+
+
 function interpreter () {
     local sprint_program=($1)
     local index=0
@@ -34,27 +80,28 @@ function interpreter () {
         then
             sprint_program=($( addition "`echo ${sprint_program[@]}`" "$index" ))
             addition_status=$?
-            if [[ ${addition_status} -eq 1 ]]
+            
+            if [[ ${addition_status} -ne 0 ]]
             then
-                echo ${sprint_program[@]}
-                return 1
+                return ${addition_status}
             fi
-            index=$(( ${index} + 2 ))
+            
+            index=$(( ${index} + 3 ))
+            continue
         fi
+
         if [[ ${sprint_program[$index]} -eq 2 ]]
         then
-            local first_index=$(( $index + 1 ))
-            local second_index=$(( $index + 2 ))
-            local third_index=$(( $index + 3 ))
-            local first_number=${sprint_program[${sprint_program[$first_index]}]}
-            local second_number=${sprint_program[${sprint_program[$second_index]}]}
-            index=$(( ${index} + 3 ))
-            if [[ ${first_number} -lt ${second_number} ]]
+            index=$( jump_if "`echo ${sprint_program[@]}`" "$index" )
+            jump_if_status=$?
+            
+            if [[ ${jump_if_status} -ne 0 ]]
             then
-                index=${sprint_program[$third_index]}
+                return ${jump_if_status}
             fi
             continue
         fi
+        
         echo ${sprint_program[@]}
         index=$(( $index + 1 ))
     done
@@ -62,15 +109,13 @@ function interpreter () {
 
 function main () {
     local sprint_program="$1"
-    # if [[ -z ${sprint_program} ]]
-    # then
-    #     echo "Code string is empty"
-    #     exit
-    # fi
+    
     interpreter "${sprint_program}"
     interpreter_status=$?
-    if [[ $interpreter_status -eq 1 ]]
+    
+    if [[ $interpreter_status -ne 0 ]]
     then
+        error_handler $interpreter_status
         exit 1
     fi
 }
